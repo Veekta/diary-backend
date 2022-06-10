9197a156ebb88cb5282e85eddd2b04af007add43
CallmeVeeTech <vsunday.1710@gmail.com>
2022-06-10 14:21:06 +0100

    files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 require("./utils/db");
-const port = process.env.PORT;
+const port = process.env.PORT || 1242;
 const app = express();
 const userRoute = require("./router/userRouter");
 const diaryRoute = require("./router/diaryRouter");
